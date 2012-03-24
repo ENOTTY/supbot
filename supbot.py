63487b12da749152f3537649d11184cc24faa957
Peter Rowlands <peter@pmrowla.com>
2012-03-23 20:43:22 -0400

    Implemented issue #1
    !seen will print the last time a nick talked. All times use the bot's
    local timezone.
    
    Also moved each command into its own "do_<cmd>" method. To add new commands
    add your function to "commands" in do_command.

--- supbot.py
+++ supbot.py
@@ -16,6 +16,7 @@ Commands:
 from ircbot import SingleServerIRCBot
 from irclib import nm_to_n, nm_to_h, irc_lower, ip_numstr_to_quad, ip_quad_to_numstr
 from collections import deque
+import time
 
 
 class SupBot(SingleServerIRCBot):
@@ -25,6 +26,7 @@ class SupBot(SingleServerIRCBot):
         SingleServerIRCBot.__init__(self, [(server, port)], nickname, nickname)
         self.channel = channel
         self.suplist = deque()
+        self.seenlist = {}
         self.maxlen = maxlen
 
     def on_nicknameinuse(self, c, e):
@@ -41,42 +43,72 @@ class SupBot(SingleServerIRCBot):
     def on_pubmsg(self, c, e):
         self.on_privmsg(c, e)
 
+        timestamp = time.localtime()
         nick = nm_to_n(e.source())
         msg = e.arguments()[0]
         self.suplist.appendleft([nick, msg])
         if len(self.suplist) > self.maxlen:
             self.suplist.pop()
+        # should possibly keep track of nick changes in the future
+        self.seenlist[nick] = timestamp
 
     def do_command(self, e, cmdlist):
         cmd = cmdlist[0]
         arglist = cmdlist[1:]
+
+        commands = {'!help': self.do_help,
+                    '!sup': self.do_sup,
+                    '!seen': self.do_seen}
+
+        try:
+            commands[cmd](e, arglist)
+        except KeyError:
+            # Unknown command
+            pass
+
+    def do_help(self, e, arglist):
+        """Send a private msg with usage info"""
         conn = self.connection
+        target = nm_to_n(e.source())
+        conn.privmsg(target, 'This bot provides a few commands for users.')
+        conn.privmsg(target, '!help - displays this message')
+        conn.privmsg(target, '!sup [COUNT] - displays the last COUNT lines of scrollback. Defaults to COUNT=20.')
+        conn.privmsg(target, '!seen NICK1 [NICK2]... - displays the last time each nick talked.')
+        conn.privmsg(target, 'This bot is Open Source. Please contribute code at https://github.com/ENOTTY/supbot')
 
-        if cmd == '!sup':
-            i = 20
-            if len(arglist) > 0:
-                try:
-                    i = int(arglist[0])
-                except ValueError:
-                    i = 20
+    def do_sup(self, e, arglist):
+        """Print some lines of scrollback"""
+        conn = self.connection
+        i = 20
+        if len(arglist) > 0:
+            try:
+                i = int(arglist[0])
+            except ValueError:
+                i = 20
 
-            rev = self.suplist
-            msgs = []
-            for v in rev:
-                msgs.append(v)
-                i -= 1
-                if i <= 0:
-                    break
+        rev = self.suplist
+        msgs = []
+        for v in rev:
+            msgs.append(v)
+            i -= 1
+            if i <= 0:
+                break
 
-            for [n,msg] in reversed(msgs):
-                conn.privmsg(nm_to_n(e.source()), "%s: %s" % (n,msg))
+        for [n,msg] in reversed(msgs):
+            conn.privmsg(nm_to_n(e.source()), "%s: %s" % (n,msg))
 
-        if cmd == '!help':
-            target = nm_to_n(e.source())
-            conn.privmsg(target, 'This bot provides a few commands for users.')
-            conn.privmsg(target, '!sup [COUNT] - displays the last COUNT lines of scrollback. Defaults to COUNT=20.')
-            conn.privmsg(target, '!help - displays this message')
-            conn.privmsg(target, 'This bot is Open Source. Please contribute code at https://github.com/ENOTTY/supbot')
+    def do_seen(self, e, arglist):
+        """Print the last time a user sent a message"""
+        conn = self.connection
+        msgs = []
+        for nick in arglist:
+            try:
+                seen = self.seenlist[nick]
+                msgs.append('%s: Last seen: %s %s' % (nick, time.asctime(seen), time.tzname[seen.tm_isdst]))
+            except KeyError:
+                msgs.append('%s: Last seen: never' % nick)
+        for msg in msgs:
+            conn.privmsg(nm_to_n(e.source()), msg)
 
 
 def main():
